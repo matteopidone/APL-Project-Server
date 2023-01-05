720e1612cc89d812476198574cd07b132944a953
Matteo Pidone <pidonematteo@hotmail.it>
2023-01-05 22:43:26 +0100

    fix(db): fix config db

--- controllers/main_controller.cc
+++ controllers/main_controller.cc
@@ -1,4 +1,6 @@
 #include "main_controller.h"
+#include <string>
+using namespace std;
 
 void main_controller::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
 {
@@ -6,13 +8,14 @@ void main_controller::asyncHandleHttpRequest(const HttpRequestPtr& req, std::fun
     auto clientPtr = drogon::app().getDbClient("Matteo");
     auto f = clientPtr->execSqlAsyncFuture("select * from users");
     auto result = f.get(); // Block until we get the result or catch the exception;
+    string a = "pippo";
     int i = 0;
     for (auto row : result)
     {
-     i++;;
+        a.assign(row["name"].as<std::string>());
     };
     resp->setStatusCode(k200OK);
     resp->setContentTypeCode(CT_TEXT_HTML);
-    resp->setBody("value is " + i);
+    resp->setBody("value is " + a);
     callback(resp);
 }
