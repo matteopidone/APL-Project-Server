6095b91aaf882365d72e0421746661aa15f8e806
Matteo Pidone <pidonematteo@hotmail.it>
2023-01-05 21:59:39 +0100

    fix(db): fix vari

--- controllers/main_controller.cc
+++ controllers/main_controller.cc
@@ -1,11 +1,18 @@
 #include "main_controller.h"
 
-void main_controller::asyncHandleHttpRequest(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
-	// write your application logic here
-	auto resp = HttpResponse::newHttpResponse();
-	// NOTE: The enum constant below is named "k200OK" (as in 200 OK), not "k2000K".
-	resp->setStatusCode(k200OK);
-	resp->setContentTypeCode(CT_TEXT_HTML);
-	resp->setBody("Hello World!");
-	callback(resp);
+void main_controller::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    auto resp=HttpResponse::newHttpResponse();
+    auto clientPtr = drogon::app().getDbClient("Matteo");
+    auto f = clientPtr->execSqlAsyncFuture("select * from users");
+    auto result = f.get(); // Block until we get the result or catch the exception;
+    int i = 0;
+    for (auto row : result)
+    {
+     i++;;
+    };
+    resp->setStatusCode(k200OK);
+    resp->setContentTypeCode(CT_TEXT_HTML);
+    resp->setBody("value is " + i);
+    callback(resp);
 }
