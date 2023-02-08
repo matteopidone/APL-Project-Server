6d79ef87c2950521f317b039915982137b424df2
Tomas Prifti <tomasprifti99@gmail.com>
2023-02-08 23:14:12 +0100

    feat(controllers): aggiunti path per il test di REST API con differenti metodi http e url

--- controllers/demo_v1_other_controller.h
+++ controllers/demo_v1_other_controller.h
@@ -12,7 +12,9 @@ class other_controller : public drogon::HttpController<other_controller>
 {
   public:
     METHOD_LIST_BEGIN
-    METHOD_ADD(other_controller::getInfo, "/{1}", Get);
+    METHOD_ADD(other_controller::getInfo, "/uno/{1}", Get);
+    METHOD_ADD(other_controller::getInfo, "/due/{1}", Post);
+    ADD_METHOD_TO(other_controller::getInfo, "/tre/{1}", Get);
     METHOD_LIST_END    
     
     void getInfo(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string param1);
