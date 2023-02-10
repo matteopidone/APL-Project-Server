68a720b18c4861478ee46025e6443eb5032bad94
Tomas Prifti <tomasprifti99@gmail.com>
2023-02-10 22:00:51 +0100

    refactor(vari file): aggiunto .editorconfig e refactor indentazione vari file

--- controllers/main_controller.h
+++ controllers/main_controller.h
@@ -4,14 +4,13 @@
 
 using namespace drogon;
 
-class main_controller : public drogon::HttpSimpleController<main_controller>
-{
-  public:
-    void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
-    PATH_LIST_BEGIN
-    // list path definitions here;
-    // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
-    PATH_ADD("/",Get,Post);
-    PATH_ADD("/test",Get);
-    PATH_LIST_END
+class main_controller : public drogon::HttpSimpleController<main_controller> {
+   public:
+	void asyncHandleHttpRequest(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) override;
+	PATH_LIST_BEGIN
+	// list path definitions here;
+	// PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
+	PATH_ADD("/", Get, Post);
+	PATH_ADD("/test", Get);
+	PATH_LIST_END
 };
