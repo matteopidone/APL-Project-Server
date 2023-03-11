e42e26ea261bae004b0759123ee36afbb7bc87d0
Tomas Prifti <tomasprifti99@gmail.com>
2023-03-11 22:59:24 +0100

    feat(user): implementazione funzioni find e get con accesso al database

--- models/User.cc
+++ models/User.cc
@@ -18,25 +18,32 @@ string User::getName() const { return this->name; }
 string User::getSurname() const { return this->surname; }
 string User::getRole() const { return this->role; }
 
+// Functions
+
+void User::create(string email, string password, string name, string surname, string role) {
+
+}
+
 bool User::find(const string &email, const string &password) {
 	drogon::orm::DbClientPtr database = drogon::app().getDbClient("Matteo");
 
-	string query = "SELECT 'name', 'surname' FROM 'users' WHERE email='" + email + "'";
+	string query = "SELECT email FROM users WHERE email='" + email + "' AND password='" + password + "'";
+	future<drogon::orm::Result> future = database->execSqlAsyncFuture(query);
+	drogon::orm::Result result = future.get();
 
-	auto f = database->execSqlAsyncFuture(query);
-	return true;
+	return (bool)result.size();
 }
 
 string * User::get(const string &email) {
 	drogon::orm::DbClientPtr database = drogon::app().getDbClient("Matteo");
 
-	string query = "SELECT 'name', 'surname' FROM 'users' WHERE email='" + email + "'";
+	string query = "SELECT name, surname FROM users WHERE email='" + email + "'";
+	future<drogon::orm::Result> future = database->execSqlAsyncFuture(query);
+	drogon::orm::Result result = future.get();
 
-	auto f = database->execSqlAsyncFuture(query);
-	auto result = f.get();
+	string * data = new string[2];
+	data[0] = result[0]["name"].as<string>();
+	data[1] = result[0]["surname"].as<string>();
 
-	string * strArray = new string[2];
-	string data[2] = {"matteo", "pidone"};
-	strArray = data;
-	return strArray;
+	return data;
 }
