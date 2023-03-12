bab1e7cc5c7c12c466b89507cd5de014429e9eac
Tomas Prifti <tomasprifti99@gmail.com>
2023-03-12 20:03:44 +0100

    feat(holiday controller): implementazione rest api per la gestione dell'invio della richiesta ferie

--- controllers/api_HolidayController.cc
+++ controllers/api_HolidayController.cc
@@ -22,12 +22,42 @@ void HolidayController::getHolidays(const HttpRequestPtr &req, std::function<voi
 		result[i]["year"] = date.tm_year;
 		result[i]["month"] = date.tm_mon;
 		result[i]["day"] = date.tm_mday;
-		result[i]["type"] = values[i].getType();
+		result[i]["type"] = stoi(values[i].getType()); // da vedere -> cambiare a intero
 		result[i]["message"] = values[i].getMessage();
 	}
 
-    HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(result);
+	HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(result);
 	resp->setStatusCode(k200OK);
 	callback(resp);
 	delete[] values;
+}
+
+void HolidayController::insertHoliday(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+	Json::Value parameters = *(req->getJsonObject());
+	string email = parameters["email"].asString();
+	int year = parameters["year"].asInt();
+	int month = parameters["month"].asInt();
+	int day = parameters["day"].asInt();
+	string message = parameters["message"].asString();
+
+	tm date = {};
+	string string_date = to_string(year) + "-" + to_string(month) + "-" + to_string(day);
+	strptime(string_date.c_str(), "%Y-%m-%d", &date);
+
+	bool res = models::Holiday::insertUserHoliday(email, date, message);
+
+	Json::Value result;
+	result["result"] = res;
+
+	if (!res) {
+		result["response"] = "Non è possibile creare la richiesta di ferie";
+		HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(result);
+		resp->setStatusCode(k500InternalServerError);
+		callback(resp);
+		return;
+	}
+
+	HttpResponsePtr resp = HttpResponse::newHttpJsonResponse(result);
+	resp->setStatusCode(k200OK);
+	callback(resp);
 }
--- controllers/api_HolidayController.h
+++ controllers/api_HolidayController.h
@@ -11,8 +11,10 @@ namespace api {
 		public:
 			METHOD_LIST_BEGIN
 			ADD_METHOD_TO(HolidayController::getHolidays, "/api/getHolidays?email={1}", Get);
+			ADD_METHOD_TO(HolidayController::insertHoliday, "/api/insertHoliday", Post);
 			METHOD_LIST_END
 
 		void getHolidays(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const string email);
+		void insertHoliday(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
 	};
 }
