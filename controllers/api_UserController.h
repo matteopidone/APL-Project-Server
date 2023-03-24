812aa431690749b6ada545ae2656fbd86eb14930
Matteo Pidone <pidonematteo@hotmail.it>
2023-03-24 18:01:28 +0100

    fix(endpoint): spostati endpoint in file constants.h

--- controllers/api_UserController.h
+++ controllers/api_UserController.h
@@ -15,10 +15,10 @@ class UserController : public drogon::HttpController<UserController>, public Aut
 {
   public:
     METHOD_LIST_BEGIN
-    ADD_METHOD_TO(UserController::login, "/api/login", Post);
-    ADD_METHOD_TO(UserController::insertUser, "/api/insertUser", Post);
-    ADD_METHOD_TO(UserController::updateRequest, "/api/updateRequest", Post);
-    ADD_METHOD_TO(UserController::getAllUserHolidays, "/api/getAllUserHolidays?email={1}", Get);
+    ADD_METHOD_TO(UserController::login, PATH_LOGIN, Post);
+    ADD_METHOD_TO(UserController::insertUser, PATH_INSERT_USER, Post);
+    ADD_METHOD_TO(UserController::updateRequest, PATH_UPDATE_REQUEST, Post);
+    ADD_METHOD_TO(UserController::getAllUserHolidays, PATH_GET_ALL_USER_HOLIDAYS, Get);
     METHOD_LIST_END
 
     void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
--- plugins/constants.h
+++ plugins/constants.h
@@ -2,4 +2,15 @@
 
 using namespace std;
 
-const string JWT_SECRET = "tokensecret";  
+//Costanti
+
+//Costante secret jwt.
+const string JWT_SECRET = "tokensecret";
+
+//Costanti path url endpoint.
+const string PATH_GET_HOLIDAYS = "/api/getHolidays?email={1}";
+const string PATH_INSERT_HOLIDAYS = "/api/insertHoliday";
+const string PATH_LOGIN = "/api/login";
+const string PATH_INSERT_USER = "/api/insertUser"; 
+const string PATH_UPDATE_REQUEST = "/api/updateRequest";
+const string PATH_GET_ALL_USER_HOLIDAYS = "/api/getAllUserHolidays?email={1}";
