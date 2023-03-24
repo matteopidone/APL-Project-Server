69b0e7f2366ecf813160aef7dad44147b06a682e
Matteo Pidone <pidonematteo@hotmail.it>
2023-03-24 17:42:37 +0100

    fix(model e controller): fixing vario

--- controllers/api_UserController.cc
+++ controllers/api_UserController.cc
@@ -66,19 +66,24 @@ void UserController::login(const HttpRequestPtr &req, std::function<void(const H
 
             bool found = models::User::find(email, password);
             result["found"] = found;
-            if( found ) {
-                string * user = models::User::get(email);
-                result["name"] = user[0];
-                result["surname"] = user[1];
-                result["email"] = email;
-
-                // Genero un JWT.
-                string jwt = generate_token(parameters, JWT_SECRET);
-                result["token"] = jwt;
-
-                // Elimino la memoria allocata.
-                delete[] user;
+            if( ! found ) {
+                //Se non trovo l'utente rispondo con status code 400.
+                resp = HttpResponse::newHttpResponse();
+                resp->setStatusCode(k400BadRequest);
+                callback(resp);
+                return;
             }
+            string * user = models::User::get(email);
+            result["name"] = user[0];
+            result["surname"] = user[1];
+            result["email"] = email;
+
+            // Genero un JWT.
+            string jwt = generate_token(parameters, JWT_SECRET);
+            result["token"] = jwt;
+
+            // Elimino la memoria allocata.
+            delete[] user;
             resp = HttpResponse::newHttpJsonResponse(result);
             resp->setStatusCode(k200OK);
         }
