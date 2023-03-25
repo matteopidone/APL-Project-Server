8fc71ceca30fc97927382f36614534bd0eac0a03
Tomas Prifti <tomasprifti99@gmail.com>
2023-03-25 21:56:06 +0100

    refactor(controllers): refactor indentazione e riordinate funzioni

--- controllers/api_UserController.cc
+++ controllers/api_UserController.cc
@@ -2,65 +2,6 @@
 
 using namespace api;
 
-void UserController::insertUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback){
-    HttpResponsePtr resp;
-    Json::Value result;
-    string auth_field = req->getHeader("Authorization");
-
-    if (!validate_token(auth_field, JWT_SECRET)) {
-		// Se non è valido rispondo con codice 401.
-		resp = HttpResponse::newHttpResponse();
-		resp->setStatusCode(HttpStatusCode::k401Unauthorized);
-		callback(resp);
-		return;
-    }
-
-    // Prendo i parametri della richiesta.
-    Json::Value parameters = *(req->getJsonObject());
-    string email = parameters["email"].asString();
-    string psw = parameters["password"].asString();
-    string name = parameters["name"].asString();
-    string surname = parameters["surname"].asString();
-    string description = parameters["description"].asString();
-    string role = parameters["role"].asString();
-
-    if( !validate_email(email) ){
-        //Se la mail non è valida rispondo con status code 400.
-		result["error"] = "Invalid email.";
-		resp = HttpResponse::newHttpJsonResponse(result);
-        resp->setStatusCode(k400BadRequest);
-        callback(resp);
-        return;
-
-    }
-
-    try {
-        bool inserted = models::User::create(email, psw, name, surname, description, role);
-
-        //Se l'utente non è stato inserito rispondo con status code 400.
-        if( !inserted ){
-            result["error"] = "User not inserted.";
-		    resp = HttpResponse::newHttpJsonResponse(result);
-            resp->setStatusCode(k400BadRequest);
-            callback(resp);
-            return;
-        }
-
-        result["inserted"] = inserted;
-        resp = HttpResponse::newHttpJsonResponse(result);
-        resp->setStatusCode(k200OK);
-        callback(resp);
-        return;
-
-    } catch (const exception &exception) {
-        //Rispondo con status code 500.
-        resp = HttpResponse::newHttpResponse();
-        resp->setStatusCode(k500InternalServerError);
-        callback(resp);
-        return;
-    }
-}
-
 void UserController::login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
     HttpResponsePtr resp;
     Json::Value result;
@@ -108,6 +49,65 @@ void UserController::login(const HttpRequestPtr &req, std::function<void(const H
 
         // Elimino la memoria allocata.
         delete[] user_info;
+        resp = HttpResponse::newHttpJsonResponse(result);
+        resp->setStatusCode(k200OK);
+        callback(resp);
+        return;
+
+    } catch (const exception &exception) {
+        //Rispondo con status code 500.
+        resp = HttpResponse::newHttpResponse();
+        resp->setStatusCode(k500InternalServerError);
+        callback(resp);
+        return;
+    }
+}
+
+void UserController::insertUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback){
+    HttpResponsePtr resp;
+    Json::Value result;
+    string auth_field = req->getHeader("Authorization");
+
+    if (!validate_token(auth_field, JWT_SECRET)) {
+		// Se non è valido rispondo con codice 401.
+		resp = HttpResponse::newHttpResponse();
+		resp->setStatusCode(HttpStatusCode::k401Unauthorized);
+		callback(resp);
+		return;
+    }
+
+    // Prendo i parametri della richiesta.
+    Json::Value parameters = *(req->getJsonObject());
+    string email = parameters["email"].asString();
+    string psw = parameters["password"].asString();
+    string name = parameters["name"].asString();
+    string surname = parameters["surname"].asString();
+    string description = parameters["description"].asString();
+    string role = parameters["role"].asString();
+
+    if( !validate_email(email) ){
+        //Se la mail non è valida rispondo con status code 400.
+		result["error"] = "Invalid email.";
+		resp = HttpResponse::newHttpJsonResponse(result);
+        resp->setStatusCode(k400BadRequest);
+        callback(resp);
+        return;
+
+    }
+
+    try {
+        bool inserted = models::User::create(email, psw, name, surname, description, role);
+
+        //Se l'utente non è stato inserito rispondo con status code 400.
+        if( !inserted ){
+            result["error"] = "User not inserted.";
+		    resp = HttpResponse::newHttpJsonResponse(result);
+            resp->setStatusCode(k400BadRequest);
+            callback(resp);
+            return;
+        }
+
+        result["inserted"] = inserted;
         resp = HttpResponse::newHttpJsonResponse(result);
         resp->setStatusCode(k200OK);
         callback(resp);
--- controllers/api_UserController.h
+++ controllers/api_UserController.h
@@ -9,22 +9,19 @@ using namespace drogon;
 using namespace std;
 using namespace aplutils;
 
-namespace api
-{
-class UserController : public drogon::HttpController<UserController>, public Utility
-{
-  public:
-    METHOD_LIST_BEGIN
-    ADD_METHOD_TO(UserController::login, PATH_LOGIN, Post);
-    ADD_METHOD_TO(UserController::insertUser, PATH_INSERT_USER, Post);
-    ADD_METHOD_TO(UserController::updateRequest, PATH_UPDATE_REQUEST, Post);
-    ADD_METHOD_TO(UserController::getAllUserHolidays, PATH_GET_ALL_USER_HOLIDAYS, Get);
-    METHOD_LIST_END
+namespace api {
+	class UserController : public drogon::HttpController<UserController>, public Utility {
+		public:
+			METHOD_LIST_BEGIN
+			ADD_METHOD_TO(UserController::login, PATH_LOGIN, Post);
+			ADD_METHOD_TO(UserController::insertUser, PATH_INSERT_USER, Post);
+			ADD_METHOD_TO(UserController::updateRequest, PATH_UPDATE_REQUEST, Post);
+			ADD_METHOD_TO(UserController::getAllUserHolidays, PATH_GET_ALL_USER_HOLIDAYS, Get);
+			METHOD_LIST_END
 
-    void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-    void updateRequest(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-    void getAllUserHolidays(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const string email);
-    void insertUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-
-};
+			void login(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+			void insertUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+			void updateRequest(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
+			void getAllUserHolidays(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, const string email);
+	};
 }
