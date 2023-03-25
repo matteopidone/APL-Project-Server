bcd6e2e82c2ff55288a9f1f3ce7304254c5c83fb
Tomas Prifti <tomasprifti99@gmail.com>
2023-03-25 21:25:06 +0100

    refactor(jwt): modificati passaggi dei parametri per valore

--- plugins/jwt.h
+++ plugins/jwt.h
@@ -10,19 +10,19 @@ using namespace std;
 namespace aplutils
 {
 /*
- * Questa classe implementa metodi per la costruzione e validazione di un Json Web Token. 
+ * Questa classe implementa metodi per la costruzione e validazione di un Json Web Token.
 */
 class JWT
 {
   private:
-    string algoritm;
+    string algorithm;
   public:
-    string decode(const string &in);
-    string encode(const string &in);
-    bool verify_jwt(const string &jwt, const string &secret);
-    string generate_jwt(const string &payload, const string &secret);
-    JWT(const string &a){
-      this->algoritm = a;
+    string decode(const string in);
+    string encode(const string in);
+    bool verify_jwt(const string jwt, const string secret);
+    string generate_jwt(const string payload, const string secret);
+    JWT(const string a) {
+      this->algorithm = a;
     };
 };
 }
