16989057cfc5a2b19e50e5f094d7f43fb1a5984a
Tomas Prifti <tomasprifti99@gmail.com>
2023-03-25 22:04:02 +0100

    refactor(plugins): refactor indentazione, ordine funzioni, commenti

--- plugins/utility.h
+++ plugins/utility.h
@@ -5,21 +5,25 @@
 
 using namespace std;
 
-namespace aplutils
-{
-/*
- * Questa classe definisce metodi di validazione e e generazione di un token, validazione mail e altri metodi di utility.
-*/
-class Utility
-{
-  private:
-    JWT jwtobj = JWT("HS256");
+namespace aplutils {
+	/**
+	 * Questa classe definisce vari metodi di utility:
+	 * - Generazione di un json web token
+	 * - Validazione del token
+	 * - Validazione email
+	 * - Validazione giorno (per richiesta ferie)
+	 * - Conversione giorno, mese, anno a tipo std::tm
+	*/
+	class Utility {
+		private:
+			JWT jwtobj = JWT("HS256");
 
-  protected:
-    bool validate_token(string auth_header, const string secret);
-    string generate_token(const Json::Value &json, const string secret);
-    bool validate_email(const string mail);
-    bool is_valid_day(tm time);
-    void parse_tm(const int day, const int month, const int year, tm &time);
-};
+		protected:
+			// Functions
+			string generate_token(const Json::Value &json, const string secret);
+			bool validate_token(string auth_header, const string secret);
+			bool validate_email(const string mail);
+			bool is_valid_day(tm time);
+			void parse_tm(const int day, const int month, const int year, tm &time);
+	};
 }
